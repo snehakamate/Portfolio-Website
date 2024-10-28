463b1672a9946ac14ef818a6ff0b6d3671dcbb07
snehakamate <162132991+snehakamate@users.noreply.github.com>
2024-10-28 12:13:17 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+const toggleButton = document.getElementById("theme-toggle");
+const body = document.body;
+
+toggleButton.addEventListener("click", () => {
+    body.classList.toggle("dark-mode");
+    
+    if (body.classList.contains("dark-mode")) {
+        toggleButton.textContent = "☀️ Light Mode";
+    } else {
+        toggleButton.textContent = "🌙 Dark Mode";
+    }
+});
